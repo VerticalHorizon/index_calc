32d16b8e22016b8d41eb1c1109691ad5289990ee
Denis Bozhenkov <mastermind@karellens.com>
2017-10-19 19:25:19 +0300

    Input from csv, out to csv, add seed file, concat to single data frame

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
 import argparse
 from math import sqrt
-from pandas import read_excel, DataFrame
+import pandas as pd
 from datetime import datetime, timedelta
 
 
 parser = argparse.ArgumentParser(description='Calculate index changes.')
 
+# available params
 parser.add_argument('-sd', '--startdate',
-                   default=datetime.strftime(datetime.now(), "%Y%m%d"),
-                   help='Start date without spaces in Ymd format. Ex: 20090114 or 2009-01-14')
-parser.add_argument('-ed', '--enddate',
                    default=datetime.strftime(datetime.now() - timedelta(weeks=4), "%Y%m%d"),
-                   help='End date')
+                   help='Start date without spaces. Ex: 20090114 or 2009-01-14')
+parser.add_argument('-ed', '--enddate',
+                   default=datetime.strftime(datetime.now(), "%Y%m%d"),
+                   help='End date without spaces. Ex: 20090114 or 2009-01-14')
 parser.add_argument('-rw', '--rollwindow',
                    default=30,
                    help='Rolling daily window')
 parser.add_argument('-f', '--filename',
                    default='seed.csv',
                    help='CSV file name')
+parser.add_argument('-o', '--outfile',
+                   default='out.csv',
+                   help='Out CSV file name')
 
 args = parser.parse_args()
 filename = args.filename
+outfile = args.outfile
 rollwindow = int(args.rollwindow)
 startdate = args.startdate
 enddate = args.enddate
 
-df = read_excel(filename, sheetname=0, header=None, skiprows=12, usecols=range(4)).set_index(0)
+df = pd.read_csv(filename, header=None).set_index(0)
 
 # sliced by date range
 sliced_df = df[startdate:enddate]
 
+# Percent change
 pct_change = sliced_df.pct_change()
-std_deviation = pct_change.std(numeric_only=True, axis=0)
+# Moving standard deviation
+std_deviation = pct_change.rolling(rollwindow).std(ddof=0).apply(lambda x: x*sqrt(220) )
 
-print pct_change.to_string()
-print pct_change.rolling(rollwindow).std(ddof=0).apply(lambda x: x*sqrt(220) ).to_string()
+out = pd.concat([pct_change, std_deviation], axis=1)
+out.to_csv(outfile, header=False)
